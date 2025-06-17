aeed62ab45e2fbd7c9d0f653b8fae0aabe4262fd
Dima Krasnov <rediskakrasnov@yandex.ru>
2025-06-17 17:32:50 +0300

    saved project

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# qa_python
+#проект содержит автоматизированные тесты для класса `BooksCollector`
+
+Cтруктура проекта:
+qa_python/
+├── main.py          # Класс BooksCollector
+├── tests.py         # Автоматизированные тесты
+├── conftest.py      # Фикстуры для тестов
+└── README.md        # Документация
+
+Функциональность класса
+Основные методы:
+add_new_book(name) — добавляет новую книгу.
+
+set_book_genre(name, genre) — устанавливает жанр книги.
+
+get_books_with_specific_genre(genre) — возвращает книги определённого жанра.
+
+get_books_for_children() — возвращает книги, подходящие для детей.
+
+add_book_in_favorites(name) — добавляет книгу в избранное.
+
+delete_book_from_favorites(name) — удаляет книгу из избранного.
--- tests.py
+++ tests.py
@@ -1,32 +1,95 @@
-import pytest
+﻿import pytest
 
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_add_two_books(self, bookscollector):
+        bookscollector.add_new_book('Гордость и предубеждение и зомби')
+        bookscollector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(bookscollector.get_books_genre()) == 2
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    @pytest.mark.parametrize('name',[
+        'Заводной апельсин: Избранные страницы!!!!',    #41 символов
+        'Мастер и Маргарита: эпилог вечной любви!!!',   #42 символов
+        'Война и мир: эпическое полотно Льва Толстого в четырёх томах'  #60 символов
+    ])
+    def test_add_new_book_when_name_book_more40_symbol_false(self, bookscollector, name):
+        bookscollector.add_new_book(name)
+        assert len(bookscollector.get_books_genre()) == 0
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-    @pytest.mark.parametrize('name',['Заводной апельсин: Избранные страницы','Мастер и Маргарита: эпилог вечной любви','Война и мир: эпическое полотно Льва Толстого в четырёх томах'])
-    def test_add_new_book_when_name_book_more40_symbol_false(self):
-    def test_add_new_book_when_name_book_40_symbol_true(self):
-    def test_add_new_book_when_name_book_less40_symbol_true(self):
-    def test_add_new_book_
-    def test_add_new_book_
+
+    @pytest.mark.parametrize('name', [
+        'Гарри Поттер и Философский Камень: Дюна',  #39 символов
+        'Гарри Поттер и Философский Камень: Лис',   #38 символов
+        'Три товарища: роман о дружбе и потерях!!',  # 40 символов
+        'Гарри Поттер и Всё',    #18 символов
+        'A' #1 символ
+    ])
+    def test_add_new_book_when_name_book_max40_symbol_true(self, bookscollector, name):
+        bookscollector.add_new_book(name)
+        assert name in bookscollector.get_books_genre()
+
+
+    def test_add_new_book_when_readding_name_book_false(self, only_one_book):
+        repeat_name = 'Физика'
+        only_one_book.add_new_book(repeat_name)
+        assert len(only_one_book.get_books_genre()) == 1
+
+
+    def test_add_new_book_when_name_null_false(self, bookscollector):
+        name = ''
+        bookscollector.add_new_book(name)
+        assert len(bookscollector.get_books_genre()) == 0
+
+
+    def test_set_book_genre_valid_true(self, bookscollector):
+        bookscollector.add_new_book("Алгебра")
+        bookscollector.set_book_genre("Алгебра", "Фантастика")
+        assert bookscollector.get_book_genre("Алгебра") == "Фантастика"
+
+
+    def test_set_book_genre_when_not_exist_genre_false(self,bookscollector):
+        bookscollector.add_new_book("Химия")
+        bookscollector.set_book_genre("Химия", "Триллер")
+        assert bookscollector.get_book_genre("Химия") == ""
+
+
+    def test_set_book_genre_when_not_exist_book_false(self, bookscollector):
+        bookscollector.set_book_genre('Физика', 'Ужасы')
+        assert len(bookscollector.get_books_genre()) == 0
+
+
+    def test_get_books_with_specific_genre_valid_true(self, collector_three_books):
+        books = collector_three_books.get_books_with_specific_genre('Ужасы')
+        assert len(books) == 2
+
+
+    def test_get_books_for_children_valid_true(self, collector_three_books):
+        children_books = collector_three_books.get_books_for_children()
+        assert len(children_books) == 1
+
+
+    def test_add_book_in_favorites_true(self, favorite_one_book):
+        assert favorite_one_book.get_list_of_favorites_books() == ['Бэтмен']
+
+
+    def test_delete_book_in_favorites_true(self, favorite_one_book):
+        favorite_one_book.delete_book_from_favorites('Бэтмен')
+        assert favorite_one_book.get_list_of_favorites_books() == []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
